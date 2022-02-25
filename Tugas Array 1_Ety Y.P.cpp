32b3184f0b451b2c0f83e4727b61233f1f622bf1
Ety yuliana putri <97831654+EtyYulianaPutri@users.noreply.github.com>
2022-02-25 16:36:15 +0700

    Add files via upload

---
+++ Tugas Array 1_Ety Y.P.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int nilai[5]={12,13,14,15,16};
+    int i;
+    for (i=0;i<5;i++){
+        cout <<"Cetak Nilai ke-"<<i+1<<" = "<<nilai[i]<<endl;
+    }
+    return 0;
+
+}
+
+ 
+
